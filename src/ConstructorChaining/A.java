8769fac604e809288b416668420e444368750c60
karthikd03 <dandikarthik2580@gmail.com>
2024-11-16 18:07:31 +0530

    first chnages

--- src/ConstructorChaining/A.java
+++ src/ConstructorChaining/A.java
@@ -1,9 +1,9 @@
 package ConstructorChaining;
 
 public class A {
-    String name;
-    A(String s){
-        name = s;
-        System.out.println("Constructor of A");
+    int name;
+    public void changingRepo()
+        {
+
     }
 }
